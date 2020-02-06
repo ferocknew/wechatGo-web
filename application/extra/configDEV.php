6a633dc16fad0e48308c57b351f3be23d8a0448a
jonah.fu <ferock@gmail.com>
2020-02-06 16:42:35 +0800

    update code

--- application/extra/configDEV.php
+++ application/extra/configDEV.php
@@ -6,4 +6,13 @@
  * Time: 上午12:11
  */
 
-return ['CDN' => ['https://golang.sg.6do.me/static/']];
+return [
+    'CDN' => ['https://golang.sg.6do.me/static/'],
+    'DATABASE' => [
+        'hostname'=>'127.0.0.1',
+        'database'=>'golang_sg_6do_me',
+        'username'=>'golang_sg_6do_me',
+        'password'=>'',
+        'hostport'=>'3306',
+    ]
+];
