7a9507997305f1b2de901453b3de107140e5ebca
jonah.fu <ferock@gmail.com>
2020-02-08 14:56:23 +0800

    session 改为file 后续会改成memcache

--- application/config.php
+++ application/config.php
@@ -216,7 +216,7 @@ return [
         'prefix' => 'think',
         // 驱动方式 支持redis memcache memcached
         // 'type' => 'memcache',
-        'type' => 'file',
+
         // 是否自动开启 SESSION
         'auto_start' => true,
         'expire' => 3600,
