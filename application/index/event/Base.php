4b3516aa76cc56e2e17443ddcc5d903ca21e9b99
jonah.fu <ferock@gmail.com>
2020-02-08 14:46:10 +0800

    增加 event 代码

---
+++ application/index/event/Base.php
@@ -0,0 +1,20 @@
+<?php
+
+namespace app\index\event;
+
+use app\index\controller;
+
+
+// 引入类库
+
+class Base extends controller\Base
+{
+
+
+    public function _initialize()
+    {
+        controller\Base::_initialize();
+    }
+
+
+}
---
+++ application/index/event/Index.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace app\index\event;
+class Index extends Base
+{
+
+    public function _initialize()
+    {
+        Base::_initialize();
+    }
+}
